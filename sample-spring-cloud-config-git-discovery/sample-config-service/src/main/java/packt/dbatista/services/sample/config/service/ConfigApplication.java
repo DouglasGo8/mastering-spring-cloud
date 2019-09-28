c64dde541e6e407690f1fadccd5fc5bce9669d99
DouglasGo8 <sgtear@hotmail.com>
2019-09-28 20:32:25 -0300

    Adjusted Configuration

---
+++ sample-spring-cloud-config-git-discovery/sample-config-service/src/main/java/packt/dbatista/services/sample/config/service/ConfigApplication.java
@@ -0,0 +1,19 @@
+package packt.dbatista.services.sample.config.service;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author dbatista
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+
+}
